6b6bf212dd4c8f6b55c31312aac50a36ee4be425
mguazzardo <mguazzardo76@gmail.com>
2021-10-10 23:45:06 -0300

    Update config.php

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 $host       = "mariadb";
 $username   = "user1";
 $password   = "master";
-$dbname     = "test1";
+$dbname     = "test";
 $dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
