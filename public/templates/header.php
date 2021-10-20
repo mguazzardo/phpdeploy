9a14890b948375ebc097cdf70ab9f039ad3a5b89
mguazzardo <mguazzardo76@gmail.com>
2021-10-20 13:52:46 -0300

    Update header.php

--- public/templates/header.php
+++ public/templates/header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Simple Database App v1</title>
+	<title>Simple Database App v2</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
